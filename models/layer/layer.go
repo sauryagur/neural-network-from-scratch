52fdc020dec10274f8f0e32135f07f7c8ec3517c
Saurya Gur <saurya.gur@outlook.com>
2025-10-16 23:10:12 +0530

    layer last input and last output caching

--- models/layer/layer.go
+++ models/layer/layer.go
@@ -2,18 +2,22 @@ package layer
 
 import (
 	"errors"
+
 	"github.com/sauryagur/neural-network-from-scratch/models/neuron"
-	)
+)
 
 type Layer struct {
-	Neurons	[]*neuron.Neuron
+	Neurons    []*neuron.Neuron
+	LastOutput []float64
+	Inputs     []float64 // this is used in backprop when this layer is the input layer
 }
 
 func (layer *Layer) Forward(inputs []float64) ([]float64, error) {
+	layer.Inputs = inputs
 	output := make([]float64, 0, len(layer.Neurons))
 
 	for i := range layer.Neurons {
-	// for each neuron
+		// for each neuron
 		neuronOutput, err := layer.Neurons[i].Forward(inputs)
 		// pass in all outputs
 		if err != nil {
@@ -22,6 +26,6 @@ func (layer *Layer) Forward(inputs []float64) ([]float64, error) {
 		}
 		output = append(output, neuronOutput)
 	}
-	
+	layer.LastOutput = output
 	return output, nil
 }
